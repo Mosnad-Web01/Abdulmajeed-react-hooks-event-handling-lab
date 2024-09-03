3bdfc56f0a2c3976136478e062f7710d1b841d16
Abdulmajeed Alselmi <hh738hhh@gmail.com>
2024-09-03 03:32:29 +0300

    solved

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
 // Code EyesOnMe Component Here
+
+import React from "react";
+
+function EyesOnMe() {
+    function handleClick() {
+        console.log("Good!");
+    }
+
+    function handleBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button
+            onClick={handleClick}
+            onBlur={handleBlur}
+        >
+            Eyes on me
+        </button>
+    );
+}
+
+export default EyesOnMe;
